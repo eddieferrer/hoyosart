ebb6cc5cacac572d6f3d07ac776f609cb7f636d3
Eddie Ferrer <eferrer@gmail.com>
2020-03-24 17:21:56 -0700

    Added blog route and sample blog

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-  mode: 'spa',
+  mode: 'universal',
   /*
    ** Headers of the page
    */
@@ -52,5 +52,17 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  generate: {
+    routes() {
+      const fs = require('fs')
+      const path = require('path')
+      return fs.readdirSync('./assets/content/blog').map((file) => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./assets/content/blog/${file}`)
+        }
+      })
+    }
   }
 }
---
+++ pages/blog/_blog.vue
@@ -0,0 +1,51 @@
+<template>
+  <article>
+    <h1>{{ blogPost.title }}</h1>
+    <div>{{ blogPost.body }}</div>
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { blogPost: payload }
+    else
+      return {
+        blogPost: await require(`~/assets/content/blog/${params.blog}.json`)
+      }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
