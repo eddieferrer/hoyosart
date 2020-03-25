fee286bdadfec6a45f0309c116b06aebcd37b620
Eddie Ferrer <eferrer@gmail.com>
2020-03-24 18:08:35 -0700

    Changed collection setup

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,23 +1,10 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="title">
-        hoyosart
-      </h1>
-      <h2 class="subtitle">
-        An artist portfolio site
-      </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <div v-for="blog in blogPosts" :key="blog.slug">
+          <nuxt-link :to="'/blog/' + blog.slug">{{ blog.title }}</nuxt-link>
+        </div>
       </div>
     </div>
   </div>
